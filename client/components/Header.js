34e54b011836beaae111242e99b1b9cc305a5006
sir1504 <sir1504@mail.ru>
2022-08-19 22:45:02 +0300

    Added login button

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,23 @@
-import { Menu } from "semantic-ui-react"
+import { Button, Menu } from "semantic-ui-react"
 import Link from "next/link"
+import { useState } from "react"
 
 const Header = () => {
+    const [currentAccount, setCurrentAccount] = useState("")
+
+    const handleLogInClick = async () => {
+        const { ethereum } = window
+        if (!ethereum) {
+            alert("у Вас нет Metamask")
+        }
+        try {
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+            setCurrentAccount(accounts[0])
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <Menu style={{ marginTop: "20px" }}>
             <Link href="/">
@@ -19,7 +35,14 @@ const Header = () => {
                     Посмотреть контакт
                 </Menu.Item>
             </Link>
-        </Menu>)
+            <Menu.Item position='right'>
+                {!currentAccount ? (<Button primary onClick={handleLogInClick}>Вход</Button>) :
+                    (<Link href="/user">
+                        <Button primary onClick={handleLogInClick}>{currentAccount}</Button>
+                    </Link>)}
+            </Menu.Item>
+        </Menu>
+    )
 }
 
 export default Header
---
+++ client/pages/user.js
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout"
+
+const userPage = () => {
+    return (<Layout>User</Layout>)
+}
+
+export default userPage
